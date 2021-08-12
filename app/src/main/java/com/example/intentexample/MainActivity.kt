6f22a906883fa745c645b3645f04dc053f8c5338
sapna73 <sg.sapna73@gmail.com>
2021-08-12 22:40:58 +0530

    Intents

---
+++ app/src/main/java/com/example/intentexample/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.intentexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btnText)
+
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java);
+            intent.putExtra("data", "test data")
+            startActivity(intent)
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+        }
+    }
+}
