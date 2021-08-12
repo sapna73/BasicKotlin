90f56e0939ebec8cafee00449e2c32df0441f262
sapna73 <sg.sapna73@gmail.com>
2021-08-13 00:25:43 +0530

    Implicit and Explicit Intents

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -10,13 +10,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val button = findViewById<Button>(R.id.btnText)
+        val button2 = findViewById<Button>(R.id.btnExplicitText)
+        val button3 = findViewById<Button>(R.id.btnImplicitText)
+        val context = this
 
         button.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java);
             intent.putExtra("data", "test data")
             startActivity(intent)
             Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+        }
+
+        button2.setOnClickListener {
+            val intent = Intent (context, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button3.setOnClickListener {
+            var intentImplicit = Intent(Intent.ACTION_SEND)
+            intentImplicit.putExtra(Intent.EXTRA_TEXT, "CodeAndroid")
+            intent.type = "text/plain"
+            startActivity(intentImplicit)
         }
     }
 }
